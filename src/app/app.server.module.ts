7de30def4dc21dadaa825e943ab3236a8968814b
haris-ash12 <haris.ash12@gmail.com>
2019-10-18 19:28:18 +0500

    a

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -9,6 +9,20 @@ import { ModuleMapLoaderModule } from "@nguniversal/module-map-ngfactory-loader"
 import { ServerCookieService } from "./services/server-cookie.service";
 import { APP_BASE_HREF } from "@angular/common";
 import { StartUpService } from "./services/start-up.service";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { NgxPaginationModule } from "ngx-pagination";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatButtonModule
+} from "@angular/material";
+import { SlideshowModule } from "ng-simple-slideshow";
+import { ScrollingModule } from "@angular/cdk/scrolling";
 
 export function setBaseHref(provider: StartUpService) {
   console.log("setBaseHref...");
@@ -27,7 +41,24 @@ export class RequestCookies {
 }
 
 @NgModule({
-  imports: [AppModule, ServerModule, ModuleMapLoaderModule],
+  imports: [
+    AppModule,
+    ServerModule,
+    ModuleMapLoaderModule,
+    HttpClientModule,
+    FormsModule,
+    NgxPaginationModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    // FlexLayoutModule,
+    SlideshowModule,
+    ScrollingModule
+  ],
   providers: [
     ServerCookieService,
     { provide: "req", useClass: RequestCookies }
